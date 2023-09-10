2c55d6378ccede4cb4a94d8c12c100f60618c570
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2023-09-10 00:01:49 -0700

    added grid, code, btn and visited or unvisited toggle

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = (country) => {
-    const {name,flags, population,area} = country.country;
+    const { name, flags, population, area, cca3 } = country.country;
     console.log(country.country);
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
     return (
         <div className='country'>
             <h3>Name: {name?.common} </h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
-           
+            <p>Code: {cca3}</p>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'unvisited'}</button>
+            { visited && 'I have visited the country.'}
         </div>
     );
 };
